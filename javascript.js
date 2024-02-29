b23c7a2b5aa7144691ec1c7aeb8adf0b79ed5fcf
UrsuTudor <tudor_ursu90@yahoo.com>
2024-02-29 20:30:00 +0200

    Add two versions of a capitalize function

--- javascript.js
+++ javascript.js
@@ -7,3 +7,24 @@ console.log(add7 (3))
 let multiply = (a, b) => a * b
 
 console.log(multiply(2, 3))
+
+function capitalize(_string) {
+    let uppercase = _string.toUpperCase();
+    let sliceFirst = uppercase.slice(0,1)
+    let sliceRest = uppercase.slice(1);
+    let lowercase = sliceRest.toLowerCase();
+    let capitalized = sliceFirst + lowercase;
+    return capitalized
+}
+
+let lowercase = 'ThIs StRiNg Is CaPiTalIzEd'
+
+console.log(capitalize(lowercase))
+
+function capitalize2(_string) {
+    let capitalizeFirst = _string.slice(0,1).toUpperCase()
+    let lowerRest = _string.slice(1).toLowerCase()
+    return capitalizeFirst + lowerRest
+}
+
+console.log(capitalize2(lowercase))
